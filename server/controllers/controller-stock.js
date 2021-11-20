35689aedb8ca2ab4fb8ed0b166192920e5105adb
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-11-20 17:32:13 +0530

    quote stock REST call done

--- server/controllers/controller-stock.js
+++ server/controllers/controller-stock.js
@@ -2,10 +2,8 @@ const axios = require('axios');
 
 module.exports.getStockQuote = async (req, res, next) => {
   try {
-    /* eslint-disable no-console */
     const { symbol } = req.params;
     const symbolize = symbol.toUpperCase();
-    console.log(symbolize);
     const { data } = await axios.get(
       `https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=${symbolize}`,
       {
@@ -14,16 +12,11 @@ module.exports.getStockQuote = async (req, res, next) => {
         },
       },
     );
-    console.log(data);
     const { quoteResponse } = data;
-    console.log(quoteResponse);
     const { result } = quoteResponse;
-    console.log(result);
     const { ask } = result[0];
     res.status(200).json({ success: true, data: ask });
   } catch (error) {
-    console.log('here');
     next(error);
-    /* eslint-enable no-console */
   }
 };
