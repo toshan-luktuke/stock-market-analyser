80abc5811e5adf9770e6c9561885a66a1b28b23d
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-11-20 16:07:24 +0530

    routing the requests

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const stock = require('./routes/router-stock');
+// const axios = require('axios');
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.use(
   }),
 );
 app.use(express.json());
+
+app.use('/stock', stock);
 
 const port = process.env.PORT;
 app.listen(port, () => {
---
+++ server/routes/router-stock.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.route('/:symbol');
+
+module.exports = router;
