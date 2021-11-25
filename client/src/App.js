330b41a7221f427a503e11c3741907c60da52397
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-11-25 23:04:46 +0530

    adding some custom fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 function App() {
-  return <h1>stonks stonks stonks</h1>
+  return (
+    <h1 className="text-5xl font-black font-curvy">stonks stonks stonks</h1>
+  )
 }
 
 export default App
