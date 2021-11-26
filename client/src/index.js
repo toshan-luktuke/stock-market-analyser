680f305d056bfc106ea5b5850936edadb26d7ab5
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-11-26 13:45:21 +0530

    app-specific context wrapper

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom'
 import { Windmill } from '@windmill/react-ui'
 import './index.css'
 import App from './App'
+import ThemedSuspense from './components/ThemedSuspense'
+import { SidebarProvider } from './context/SidebarContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Windmill usePreferences>
-      <App />
-    </Windmill>
+    <SidebarProvider>
+      <Suspense fallback={<ThemedSuspense />}>
+        <Windmill usePreferences>
+          <App />
+        </Windmill>
+      </Suspense>
+    </SidebarProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
