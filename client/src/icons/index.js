483c0b04543979d83837c7e4e587c5be4f219b19
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-11-26 13:53:20 +0530

    adding icons and icon exports

---
+++ client/src/icons/index.js
@@ -0,0 +1,7 @@
+import { ReactComponent as HomeIcon } from './home.svg'
+import { ReactComponent as ForbiddenIcon } from './forbidden.svg'
+import { ReactComponent as SearchIcon } from './search.svg'
+import { ReactComponent as SunIcon } from './sun.svg'
+import { ReactComponent as MoonIcon } from './moon.svg'
+
+export { HomeIcon, ForbiddenIcon, SearchIcon, SunIcon, MoonIcon }
