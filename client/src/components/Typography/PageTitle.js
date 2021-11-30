8e5487d184a91aa11b3c0e58dcd68662db2401b9
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-11-30 16:35:54 +0530

    added in some (meaningless) animations coz why not

--- client/src/components/Typography/PageTitle.js
+++ client/src/components/Typography/PageTitle.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import 'animate.css';
 
 const PageTitle = ({ children }) => {
   return (
-    <h1 className="my-6 text-3xl lg:text-4xl font-black text-gray-700 dark:text-gray-200 font-head text-center tracking-widest">
+    <h1 className="my-6 text-3xl lg:text-4xl font-black text-gray-700 dark:text-gray-200 font-head text-center tracking-widest animate__animated animate__backInDown">
       {children}
     </h1>
   );
