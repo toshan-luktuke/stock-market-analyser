5d4b4995a57bd3ffee2aee6d0e4ef014f3c4268e
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-12-02 00:51:45 +0530

    basic autosuggest setup working

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,93 +1,48 @@
-import React from 'react';
-import {
-  ResponsiveContainer,
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-} from 'recharts';
-
+import React, { useState, useEffect } from 'react';
+import { get } from 'axios';
 import CTA from '../components/CTA';
 import PageTitle from '../components/Typography/PageTitle';
-import SectionTitle from '../components/Typography/SectionTitle';
 
 const Forms = () => {
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  const [stock, setStock] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  const getSuggestions = async (url) => {
+    const { data } = await get(url);
+    setSuggestions(data);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStock('');
+  };
+
+  useEffect(() => {
+    const url = `https://financialmodelingprep.com/api/v3/search?query=${stock}&limit=10&apikey=${API_KEY}`;
+    getSuggestions(url);
+    console.log(suggestions);
+  }, [stock]);
+
   return (
     <>
-      <PageTitle>Search Any Stock Symbol</PageTitle>
-      <div className="w-full">
-        <ResponsiveContainer width="100%" height={250}>
-          <AreaChart
-            data={data}
-            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-          >
-            <XAxis dataKey="name" />
-            <YAxis />
-            <CartesianGrid strokeDasharray="3 3" />
-            <Tooltip />
-            <Area
-              type="monotone"
-              dataKey="uv"
-              stroke="#8884d8"
-              fillOpacity={1}
-              fill="url(#colorUv)"
-            />
-            <Area
-              type="monotone"
-              dataKey="pv"
-              stroke="#82ca9d"
-              fillOpacity={1}
-              fill="url(#colorPv)"
-            />
-          </AreaChart>
-        </ResponsiveContainer>
-      </div>
+      <PageTitle>Search Any Stock</PageTitle>
+      <form onSubmit={handleSubmit} className="flex justify-between p-2 my-8">
+        <input
+          type="text"
+          id="stock"
+          name="stock"
+          value={stock}
+          onChange={(e) => setStock(e.target.value)}
+          className="w-full bg-cool-gray-200 dark:bg-cool-gray-800 rounded-lg py-2 px-4 dark:text-white"
+          placeholder="Enter any stock name or symbol like AAPL, Tesla"
+        />
+        <button
+          type="submit"
+          className="mx-2 p-2 text-white bg-purple-600 dark:bg-green-700 rounded-md focus:border-blue-300"
+        >
+          Search
+        </button>
+      </form>
       <CTA />
     </>
   );
