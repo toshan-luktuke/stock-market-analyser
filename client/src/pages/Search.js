27dff294b531a018a3e18b0347e86d8755acb124
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-12-03 00:35:40 +0530

    autosuggest search bar complete

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -14,19 +14,26 @@ const Forms = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setStock('');
   };
 
   useEffect(() => {
-    const url = `https://financialmodelingprep.com/api/v3/search?query=${stock}&limit=10&apikey=${API_KEY}`;
-    getSuggestions(url);
-    console.log(suggestions);
+    const url = `https://financialmodelingprep.com/api/v3/search-name?query=${stock}&limit=10&exchange=NASDAQ&apikey=${API_KEY}`;
+    if (stock.length === 0) {
+      setSuggestions([]);
+    }
+    if (stock.length >= 1) {
+      getSuggestions(url);
+    }
   }, [stock]);
 
   return (
     <>
       <PageTitle>Search Any Stock</PageTitle>
-      <form onSubmit={handleSubmit} className="flex justify-between p-2 my-8">
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-between p-2 mt-8 mb-0"
+        autoComplete="off"
+      >
         <input
           type="text"
           id="stock"
@@ -35,15 +42,45 @@ const Forms = () => {
           onChange={(e) => setStock(e.target.value)}
           className="w-full bg-cool-gray-200 dark:bg-cool-gray-800 rounded-lg py-2 px-4 dark:text-white"
           placeholder="Enter any stock name or symbol like AAPL, Tesla"
+          onBeforeInput={() => setSuggestions([])}
         />
         <button
           type="submit"
           className="mx-2 p-2 text-white bg-purple-600 dark:bg-green-700 rounded-md focus:border-blue-300"
+          onClick={() => setSuggestions([])}
         >
           Search
         </button>
       </form>
-      <CTA />
+      {suggestions &&
+        stock.length > 0 &&
+        suggestions.map((suggestion) => {
+          return (
+            <div
+              key={suggestion.symbol}
+              className="bg-cool-gray-200 dark:bg-cool-gray-800 dark:text-gray-200 py-2 px-4 ml-2 mr-24 cursor-pointer bg-opacity-50 hover:bg-gray-300 dark:hover:bg-cool-gray-600"
+              onClick={() => {
+                setStock(suggestion.name);
+                setSuggestions((prev) => {
+                  return [];
+                });
+              }}
+              onBlur={() => {
+                {
+                  setTimeout(() => {
+                    setSuggestions([]);
+                  }, 100);
+                }
+              }}
+            >
+              <p className="text-sm">{suggestion.symbol}</p>
+              <p className="text-xs">{suggestion.name}</p>
+            </div>
+          );
+        })}
+      <div className="mt-8">
+        <CTA />
+      </div>
     </>
   );
 };
