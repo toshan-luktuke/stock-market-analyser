8170a141247e14ee1178aba439ea095d7a1a04c9
Rushabh Maru <rushabh.maru123@gmail.com>
2021-12-14 22:42:54 +0530

    test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import requests
 # set base url
 BASE = 'http://127.0.0.1:5000/'
 
-inp = input("Enter name code: ")
+inp = input("Enter name code:  ")
 
 response = requests.post(BASE + f'stock/{inp}')
 print(response.json())
