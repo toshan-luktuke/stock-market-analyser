24c09a21a1021a9bc7833e0c55d8988e0d7eccf7
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-01-10 23:05:56 +0530

    folder collisions removed

--- model/routes.py
+++
@@ -1,4 +0,0 @@
-# This file is contains the routes and connects api to html frontend
-
-from flask import render_template, redirect, url_for, flash, request
-# Also maybe create an __init__ file
--- model/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# set base url
-BASE = 'http://127.0.0.1:5000/'
-
-inp = input("Enter name code:  ")
-
-response = requests.post(BASE + f'stock/{inp}')
-print(response.json())
