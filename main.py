e3461d1e1f9555294fc5808699de7304a831933f
Rushabh Maru <rushabh.maru123@gmail.com>
2022-01-11 21:51:25 +0530

    change

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api, Resource, abort
 from Model.linear_reg_model import find_r
 from Model.ann_model import find
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 api = Api(app)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 class Stock_Reg(Resource):
@@ -14,7 +17,9 @@ class Stock_Reg(Resource):
             res = find_r(stock_name)
             if res == -1:
                 abort(404, message="Stock code is invalid")
-            return {'data': res}
+            response = jsonify(res)
+            response.headers.add("Access-Control-Allow-Origin", "*")
+            return response
         except:
             print("SERVER ERROR 500")
 
@@ -25,7 +30,9 @@ class Stock_ANN(Resource):
             res = find(stock_name)
             if res == -1:
                 abort(404, message="Stock code is invalid")
-            return {'data': str(res)}
+            response = jsonify(str(res))
+            response.headers.add("Access-Control-Allow-Origin", "*")
+            return response
         except:
             print("SERVER ERROR 500")
 
