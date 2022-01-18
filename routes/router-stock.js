6314d5fa3e670c8028e0ce1e30b494fdbd05863b
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-01-18 14:04:55 +0530

    backend update, ready for production

--- routes/router-stock.js
+++ routes/router-stock.js
@@ -10,6 +10,15 @@ const {
   getStockDetails,
   getSocialSentiments,
   getRating,
+  getIndexQuote,
+  getIndexChart,
+  getPredSuggestion,
+  getNewAutoSuggestions,
+  isOpen,
+  indianAutoSuggest,
+  isIndianOpen,
+  getChartIndia,
+  getIndianStockChart,
 } = require('../controllers/controller-stock');
 
 router.route('/quote/:symbol').get(getStockQuote);
@@ -20,5 +29,15 @@ router.route('/wsb').get(getWSB);
 router.route('/details/:symbol').get(getStockDetails);
 router.route('/social/:symbol').get(getSocialSentiments);
 router.route('/rating/:symbol').get(getRating);
+router.route('/index/quote/:symbol').get(getIndexQuote);
+router.route('/index/chart/:symbol/:start/:end').get(getIndexChart);
+// cut down on the response size
+router.route('/predautosuggest/:name').get(getPredSuggestion);
+router.route('/newautosuggest/:name').get(getNewAutoSuggestions);
+router.route('/isopen').get(isOpen);
+router.route('/indian/autosuggest/:name').get(indianAutoSuggest);
+router.route('/indian/isopen').get(isIndianOpen);
+router.route('/indian/index/:symbol').get(getChartIndia);
+router.route('/indian/chart/:symbol').get(getIndianStockChart);
 
 module.exports = router;
---
+++ tick.js
@@ -0,0 +1,10 @@
+module.exports.tick = [
+  { symbol: 'AAPL', name: 'Apple Inc. Common Stock' },
+  { symbol: 'FB', name: 'Meta Platforms Inc. Class A Common Stock' },
+  { symbol: 'GOOG', name: 'Alphabet Inc. Class C Capital Stock' },
+  { symbol: 'GOOGL', name: 'Alphabet Inc. Class A Common Stock' },
+  { symbol: 'RELI', name: 'Reliance Global Group Inc. Common Stock' },
+  { symbol: 'SPY', name: 'S&P 500 ' },
+  { symbol: 'TSLA', name: 'Tesla Inc. Common Stock' },
+  { symbol: 'TTM', name: 'Tata Motors Ltd Tata Motors Limited' },
+];
